a6c744be48d32967e715c106edb0f2b26a9b4663
Patrik Lamberger <apputvecklaren@gmail.com>
2015-05-29 21:16:12 +0200

    Init
    Start

---
+++ js/pjlHideSho.js
@@ -0,0 +1,9 @@
+//Show message and fade out
+var hiddenBox = $( "#banner-message" );
+var hideBtn = $( "#button-container" )
+$( "#button-container a" ).on( "click", function( event ) {
+  hiddenBox.show();
+  hideBtn.hide();
+  hiddenBox.fadeOut(7000);
+  hideBtn.delay(7000).fadeIn();
+});
---
+++ js/pjltoogle.js
@@ -0,0 +1,4 @@
+//Toogle message with a link
+$( ".view a" ).click(function() {
+	$( ".mess" ).toggle( "show" );
+});
