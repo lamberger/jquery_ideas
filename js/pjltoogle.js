2e7f93d9ef5ebbd68b461dea13b1e4ae9b0bba81
Patrik Lamberger <apputvecklaren@gmail.com>
2015-05-30 09:03:40 +0200

    Update jQ toggle
    Change html text from link depending on toggle state

--- js/pjltoogle.js
+++ js/pjltoogle.js
@@ -1,4 +1,25 @@
 //Toogle message with a link
-$( ".view a" ).click(function() {
-	$( ".mess" ).toggle( "show" );
+// $( ".view a" ).click(function() {
+// 	$( ".mess" ).toggle( "show" );
+// });
+
+
+$('.view a').click(function() {
+
+    jQuery(this).text('View code');
+
+		if($('.mess').is(':visible')) {
+
+    	jQuery(this).text('View code');
+
+    }
+		else {
+
+      jQuery(this).text('Hide code');
+
+    }
+
+		$('.mess').toggle('show');
+
+    return false;
 });
